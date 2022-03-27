179e793babf6d7f0138d26ac131f9bc643c2e7a7
PurvaYadav <94224975+PurvaYadav@users.noreply.github.com>
2022-03-27 11:38:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(150,90,40,0,2 * Math.PI);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(250,90,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(350,90,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(200,135,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(300,135,40,0,2 * Math.PI);
+ctx.stroke();
